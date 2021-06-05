6b05d298e906db1049caed3895ba629e822e5ad9
ahsanarique <ahsan.arique@gmail.com>
2021-06-05 22:46:37 +0600

    removed props

--- src/App.js
+++ src/App.js
@@ -1,62 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Switch, Route } from "react-router-dom";
-import fakeData from "./Fakedata/fakedata";
 import Header from "./Components/Header/Header";
 import Cart from "./Components/Cart/Cart.js";
 import Photos from "./Components/Photos/Photos";
 
 function App() {
-  const [allPhotos, setAllPhotos] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
-
-  useEffect(() => {
-    setAllPhotos(fakeData);
-  }, []);
-
-  function toggleFavorite(id) {
-    const updatedArr = allPhotos.map((photo) => {
-      if (photo.id === id) {
-        return { ...photo, isFavorite: !photo.isFavorite };
-      }
-      return photo;
-    });
-
-    setAllPhotos(updatedArr);
-  }
-
-  function addToCart(newItem) {
-    setCartItems((prevItems) => [...prevItems, newItem]);
-  }
-
-  function removeFromCart(id) {
-    setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
-  }
-
-  function emptyCart() {
-    setCartItems([]);
-  }
-
   return (
     <div>
-      <Header cartItems={cartItems} />
+      <Header />
       <Switch>
         <Route exact path="/">
-          <Photos
-            allPhotos={allPhotos}
-            cartItems={cartItems}
-            setAllPhotos={setAllPhotos}
-            toggleFavorite={toggleFavorite}
-            addToCart={addToCart}
-            removeFromCart={removeFromCart}
-          />
+          <Photos />
         </Route>
 
         <Route path="/cart">
-          <Cart
-            cartItems={cartItems}
-            emptyCart={emptyCart}
-            removeFromCart={removeFromCart}
-          />
+          <Cart />
         </Route>
       </Switch>
     </div>
